07e3a90dd6dfdeb127f998f538b2e82ef2b8f98d
WitnessPrune <158563210+WitnessPrune@users.noreply.github.com>
2024-02-13 02:43:52 +0530

    .

--- wprn_vgg_c10_fish.py
+++ wprn_vgg_c10_fish.py
@@ -725,7 +725,7 @@ if __name__ == '__main__':
     transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transf
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
 
   
